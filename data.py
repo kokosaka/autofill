1f0d45c577af26fd0b367a94b0deca913febac91
Koboh Kosaka <kobohkosaka@outlook.com>
2021-08-09 17:46:47 -0700

    Update README

--- data.py
+++ data.py
@@ -3,6 +3,4 @@ price = "1300"
 zip = "90503"
 desc = "this is a camera bought with cragilist"
 email = "yo@gmail.com"
-image = ["/Users/kobohkosaka/Desktop/pyth_autofill/prod_images/download.jpeg",
-         '/Users/kobohkosaka/Desktop/pyth_autofill/prod_images/download (1).jpeg',
-         "/Users/kobohkosaka/Desktop/pyth_autofill/prod_images/download.png"]
+image = [""]
